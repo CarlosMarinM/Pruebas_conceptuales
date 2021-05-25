6b561bb9f015777b901ee9fb1eb615d046714e80
carlos.marinm@hotmail.com <carlos.marinm@hotmail.com>
2021-05-25 07:12:26 -0500

    Flyway-plugin-maven

---
+++ Flyway-plugin-maven/flyway/src/main/resources/db/migration/V1.1__Insert_Records.sql
@@ -0,0 +1,12 @@
+INSERT INTO "flyway_test_table" (ID,FIRSTNAME,LASTNAME,MAIL) VALUES 
+(1,'Anthony','Autumn','egestas.nunc@elitNulla.net'),
+(2,'Bert','Phoebe','a@SednequeSed.com'),
+(3,'Abel','Mikayla','Duis.elementum@aliquetProinvelit.ca'),
+(4,'Merrill','Emerald','tortor@tinciduntnunc.com'),
+(5,'Tanner','Demetria','sit.amet.nulla@non.edu'),
+(6,'Addison','Sacha','aliquet.Proin@Phasellusfermentum.ca'),
+(7,'Amos','Cheyenne','nonummy.Fusce@ornarelectus.ca'),
+(8,'Zephania','Grace','Donec.est@gravidasit.net'),
+(9,'Ira','Maisie','purus.Duis@utaliquam.co.uk'),
+(10,'Dennis','Rhoda','eleifend.nunc.risus@ultrices.net'),
+(11,'Carlos','Marin','carlos.marin@ultrices.net');
---
+++ Flyway-plugin-maven/flyway/src/main/resources/db/migration/V1__Create_Table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "flyway_test_table" (
+ID SERIAL PRIMARY KEY,
+FIRSTNAME varchar(255) default NULL,
+LASTNAME varchar(255) default NULL,
+MAIL varchar(255) default NULL
+);

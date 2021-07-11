9ad32004fc390a7e5fd198137faac6e5c347420e
carlos.marinm@hotmail.com <carlos.marinm@hotmail.com>
2021-07-11 11:37:36 -0500

    Prueba de empaquetado de aplicacion React en Spring Boot

---
+++ React_SpringBoot_package/spring-front-end/src/main/java/com/carlos/react/SpringFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrontEndApplication.class, args);
+	}
+
+}
---
+++ React_SpringBoot_package/spring-front-end/src/test/java/com/carlos/react/SpringFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
